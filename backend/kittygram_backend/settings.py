aae31c7be85bb5b64cee7edac86b59b9a2fb413a
Ubuntu <ubuntu@fv4blk8mtq3l15it5tdt.auto.internal>
2026-05-17 00:23:42 +0000

    v.3.0

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-123')
 
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.227.112', '127.0.0.1', 'localhost', 'kittygramartrus.servehttp.com']
 
